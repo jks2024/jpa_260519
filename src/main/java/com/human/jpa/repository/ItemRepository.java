5c48f55775da62f652b31810b37617d3f591b134
jks2024 <jks2024@gmail.com>
2026-05-20 13:48:55 +0900

    item 추가3

--- src/main/java/com/human/jpa/repository/ItemRepository.java
+++ src/main/java/com/human/jpa/repository/ItemRepository.java
@@ -1,5 +1,6 @@
 package com.human.jpa.repository;
 
+import com.human.jpa.constant.ItemSellStatus;
 import com.human.jpa.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -31,5 +32,25 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     // Like : SQL의 LIKE 연산자와 동일하게 패턴을 사용하여 조회
     // OrderBy : 정렬 조건을 지정
 
+
+    // 문제 1. 상품명이 "테스트 상품5"인 상품을 조회하는 쿼리 메서드를 작성하고 테스트하시오.
     List<Item> findByItemName(String itemName);
+    // 문제 2. 상품명이 "테스트 상품1" 이거나 상세설명이 "테스트 상품 상세 설명2"인 상품을 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> findByItemNameOrItemDetail(String itemName, String itemDetail);
+    // 문제 3. 가격이 50000원 미만인 상품을 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> findByPriceLessThan(int price);
+    // 문제 4. 가격이 50000원 미만인 상품을 가격 내림차순으로 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> findByPriceLessThanOrderByPriceDesc(int price);
+    // 문제 5. 상세설명에 "상세 설명1"이라는 키워드가 포함된 상품을 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> findByItemDetailContaining(String itemDetail);
+    // 문제 6. 판매 상태가 SELL인 상품 목록과 SOLD_OUT인 상품 목록을 각각 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> findByItemSellStatus(ItemSellStatus itemSellStatus);
+    // 문제 7. 가격이 30000원 이상 70000원 이하인 상품을 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> findByPriceBetween(int startPrice, int endPrice);
+    // 문제 8. 판매 상태가 SELL인 상품의 개수와 SOLD_OUT인 상품의 개수를 각각 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> countByItemSellStatus(ItemSellStatus itemSellStatus);
+    // 문제 9. 상품명 "테스트 상품1"이 존재하는지 여부와 "없는 상품"이 존재하는지 여부를 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    boolean existsByItemName(String itemName);
+    // 문제 10. 재고가 50 미만인 상품을 재고 오름차순으로 조회하는 쿼리 메서드를 작성하고 테스트하시오.
+    List<Item> findByStockNumberLessThanOrderByStockNumberAsc(int stockNumber);
 }
