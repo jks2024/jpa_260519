1aeee0817543779d4f75caa96a799f736552382d
jks2024 <jks2024@gmail.com>
2026-05-20 11:47:33 +0900

    item 추가2

--- src/main/java/com/human/jpa/repository/ItemRepository.java
+++ src/main/java/com/human/jpa/repository/ItemRepository.java
@@ -4,6 +4,8 @@ import com.human.jpa.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     // 기본적인 CRUD는 포함되어 있음
@@ -12,4 +14,22 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     // findAll() : 모든 엔티티 조회 SELECT * from item
     // delete(id): ID로 엔티티 삭제
     // count() : 전체 엔티티 수 반환
+    // 상품 이름으로 조회
+
+    // 쿼리메서드 : Spring Data JPA에서는 메서드 이름을 통해 쿼리를 자동으로 생성할 수 있음
+    // 형식 : [메서드 키워드] + By + [조건 필드] + [추가 조건]
+    // 1. findBy : 특정 조건에 따라 데이터를 조회
+    // 2. countBy : 특정 조건에 해당하는 데이터의 개수를 조회할 때 사용
+    // 3. existsBy : 특정 조건에 해당하는 데이터가 존재하는 지 여부를 조회할 때 사용
+    // 4. deleteBy : 특정 조건에 따라 데이터를 삭제 할 때 사용
+
+    // 주요키워드
+    // And : 여러 조건을 AND로 연결
+    // Or : 여러 조건을 OR로 연결
+    // Between : 특정 범위 내에 있는 데이터를 조회
+    // LessThan, GreaterThan** : 크거나 작은 값을 비교하여 조회
+    // Like : SQL의 LIKE 연산자와 동일하게 패턴을 사용하여 조회
+    // OrderBy : 정렬 조건을 지정
+
+    List<Item> findByItemName(String itemName);
 }
--- src/test/java/com/human/jpa/repository/ItemRepositoryTest.java
+++ src/test/java/com/human/jpa/repository/ItemRepositoryTest.java
@@ -1,14 +1,49 @@
 package com.human.jpa.repository;
 
+import com.human.jpa.constant.ItemSellStatus;
+import com.human.jpa.entity.Item;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @SpringBootTest
+@Slf4j
 @TestPropertySource("classpath:application-test.properties")
 class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;  // 필드를 통한 의존성 주입
+
+    @Test
+    @DisplayName("상품저장 테스트")
+    public void saveTest() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemName("테스트 상품" + i);
+            item.setPrice(10000);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+    }
+    // 상품 조회 테스트
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNameTest() {
+        saveTest();
+        List<Item> list = itemRepository.findByItemName("테스트 상품5");
+        for (Item item : list) {
+            System.out.println(item);
+        }
+    }
 
 
 }
