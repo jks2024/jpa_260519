d0e842ed492e69c4cf3484f4ff8aec11fd8cd7fb
jks2024 <jks2024@gmail.com>
2026-05-21 12:48:35 +0900

    주문서2

--- src/main/java/com/human/jpa/entity/Order.java
+++ src/main/java/com/human/jpa/entity/Order.java
@@ -1,18 +1,28 @@
 package com.human.jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.human.jpa.constant.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "orders")
 public class Order {
     // order_id PK
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
 
     // N:1 회원 연관관계 매핑
-
-    // 주문일
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     // enum 타입으로 주문 상태
+    private OrderStatus orderStatus;
 
     // 등록일, 수정일
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
 }
--- src/main/java/com/human/jpa/entity/OrderItem.java
+++ src/main/java/com/human/jpa/entity/OrderItem.java
@@ -1,9 +1,11 @@
 package com.human.jpa.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -11,13 +13,25 @@ import lombok.ToString;
 @ToString
 public class OrderItem {
     // order_item_od PK
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
+    private Long id;
 
     // N:1 연관 관계 매핑, 한개의 아이템은 주문서 내에 여러 아이템으로 사용 될 수 있음
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 
     // N:1, 한개의 주문서에 여러개의 OrderItem 이 있을 수 있음
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
 
     private int orderPrice;
     private int quantity;
     // 등록일과 수정일
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
 
 }
